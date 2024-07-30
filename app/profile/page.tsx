a4d60eaf1dae04c7da981d88469bf397d0c14a90
Ben Lucas <124517145+retcon1@users.noreply.github.com>
2024-07-30 18:23:10 +0100

    REFACTOR: changes update function to only run once a day to avoid unnecessary loading times, restyles success toast to account for more text

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -7,22 +7,47 @@ import { UserShowStats } from "../interfaces/interfaces";
 import Link from "next/link";
 import UserShowTile from "../components/UserShowTile";
 import { updateShows } from "../utils/updateFunctions";
+import Success from "../components/toasts/Success";
 
 const Profile = () => {
   const [userShows, setUserShows] = useState<UserShowStats[]>([]);
   const [loading, setLoading] = useState(true);
   const [order, setOrder] = useState({ by: "title", asc: true });
+  const [notification, setNotification] = useState("");
+  const [popup, setPopup] = useState(false);
 
   useEffect(() => {
-    onAuthStateChanged(auth, async () => {
-      const currentShows = await getShowsBy(order.by, order.asc);
-      if (currentShows) {
-        setUserShows(currentShows);
-        setLoading(false);
+  onAuthStateChanged(auth, async () => {
+    const currentShows = await getShowsBy(order.by, order.asc);
+    if (currentShows) {
+      setUserShows(currentShows);
+      setLoading(false);
+    }
+
+    // Check if the user has any shows with new episodes once a day
+    const lastUpdate = localStorage.getItem('lastUpdate');
+    const now = new Date().getTime();
+    const oneDay = 24 * 60 * 60 * 1000; // 24 hours in milliseconds
+
+    if (!lastUpdate || now - parseInt(lastUpdate) > oneDay) {
+      const changedShows = await updateShows();
+
+      if (changedShows && changedShows.length > 0) {
+        let text = "These shows have new episodes: ";
+        changedShows.forEach((show, index) => {
+          text += `${show}${index < changedShows.length - 1 ? ', ' : '.'}`;
+        });
+        setNotification(text);
+        setPopup(true);
+        setTimeout(() => {
+          setPopup(false);
+        }, 10000);
       }
-      updateShows();
-    });
-  }, [order]);
+
+      localStorage.setItem('lastUpdate', now.toString());
+    }
+  });
+}, [order]);
 
   if (loading) {
     return (
@@ -49,78 +74,81 @@ const Profile = () => {
   }
 
   return (
-    <div className="overflow-x-auto sm:px-12 md:px-24 lg:px-48 xl:px-60">
-      <table className="table">
-        <thead>
-          <tr>
-            {/* <th></th> */}
-            <th
-              className="hover:cursor-pointer hover:text-white"
-              onClick={() => setOrder({ by: "title", asc: !order.asc })}
-            >
-              Title
-            </th>
-            <th
-              className="p-0 hover:cursor-pointer hover:text-white sm:p-3"
-              onClick={() => setOrder({ by: "rating", asc: !order.asc })}
-            >
-              Rating
-            </th>
-            <th
-              className="hover:cursor-pointer hover:text-white"
-              onClick={() =>
-                setOrder({ by: "total_episodes", asc: !order.asc })
-              }
-            >
-              Progress
-            </th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th className="font-bold text-primary">Watching</th>
-          </tr>
-          {userShows
-            .filter((show) => show.status == "watching")
-            .map((show) => {
-              return <UserShowTile key={show.id} showDetails={{ ...show }} />;
-            })}
-          <tr>
-            <th className="font-bold text-success">Completed</th>
-          </tr>
-          {userShows
-            .filter((show) => show.status == "completed")
-            .map((show) => {
-              return <UserShowTile key={show.id} showDetails={{ ...show }} />;
-            })}
-          <tr>
-            <th className="font-bold text-warning">Planning</th>
-          </tr>
-          {userShows
-            .filter((show) => show.status == "planning")
-            .map((show) => {
-              return <UserShowTile key={show.id} showDetails={{ ...show }} />;
-            })}
-          <tr>
-            <th className="font-bold text-error">Dropped</th>
-          </tr>
-          {userShows
-            .filter((show) => show.status == "dropped")
-            .map((show) => {
-              return <UserShowTile key={show.id} showDetails={{ ...show }} />;
-            })}
-          <tr>
-            <th className="font-bold text-secondary">Paused</th>
-          </tr>
-          {userShows
-            .filter((show) => show.status == "paused")
-            .map((show) => {
-              return <UserShowTile key={show.id} showDetails={{ ...show }} />;
-            })}
-        </tbody>
-      </table>
-    </div>
+    <>
+      {popup && <Success text={notification} />}
+      <div className="overflow-x-auto sm:px-12 md:px-24 lg:px-48 xl:px-60">
+        <table className="table">
+          <thead>
+            <tr>
+              {/* <th></th> */}
+              <th
+                className="hover:cursor-pointer hover:text-white"
+                onClick={() => setOrder({ by: "title", asc: !order.asc })}
+              >
+                Title
+              </th>
+              <th
+                className="p-0 hover:cursor-pointer hover:text-white sm:p-3"
+                onClick={() => setOrder({ by: "rating", asc: !order.asc })}
+              >
+                Rating
+              </th>
+              <th
+                className="hover:cursor-pointer hover:text-white"
+                onClick={() =>
+                  setOrder({ by: "total_episodes", asc: !order.asc })
+                }
+              >
+                Progress
+              </th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th className="font-bold text-primary">Watching</th>
+            </tr>
+            {userShows
+              .filter((show) => show.status == "watching")
+              .map((show) => {
+                return <UserShowTile key={show.id} showDetails={{ ...show }} />;
+              })}
+            <tr>
+              <th className="font-bold text-success">Completed</th>
+            </tr>
+            {userShows
+              .filter((show) => show.status == "completed")
+              .map((show) => {
+                return <UserShowTile key={show.id} showDetails={{ ...show }} />;
+              })}
+            <tr>
+              <th className="font-bold text-warning">Planning</th>
+            </tr>
+            {userShows
+              .filter((show) => show.status == "planning")
+              .map((show) => {
+                return <UserShowTile key={show.id} showDetails={{ ...show }} />;
+              })}
+            <tr>
+              <th className="font-bold text-error">Dropped</th>
+            </tr>
+            {userShows
+              .filter((show) => show.status == "dropped")
+              .map((show) => {
+                return <UserShowTile key={show.id} showDetails={{ ...show }} />;
+              })}
+            <tr>
+              <th className="font-bold text-secondary">Paused</th>
+            </tr>
+            {userShows
+              .filter((show) => show.status == "paused")
+              .map((show) => {
+                return <UserShowTile key={show.id} showDetails={{ ...show }} />;
+              })}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 };
 
